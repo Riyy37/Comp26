3017df3ede5e2ed36ac21affc19534ce7b26b802
Riyy37 <domabruho123@gmail.com>
2025-02-24 18:39:42 +0200

    Task3

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    /// <summary>
+    /// Заполнить одномерный массив соответствующими индексами и вывести с использованием цикла foreach.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] mas = new int[10];
+
+            Foreach(mas);
+
+        }
+        static void Foreach(int[] mas)
+        {
+            For(mas);
+            foreach (int value in mas)
+            {
+                Console.WriteLine(value);
+            }
+        }
+        static void For(int[] mas)
+        {
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = i;
+            }
+        }
+    }
+}
